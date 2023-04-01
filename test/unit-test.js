c8385a965c865e766586c89ef2f5ec251f9fe5b8
jaimebarrancos <jaime.barrancos@outlook.com>
2023-04-01 22:46:39 +0100

    Added unit tests
    Changed wsl version from 2 to 1 so now "yarn hardhat test" is faster! (from 70 seconds to 5)
    Added characters to wrap to 100 in prettier.

--- test/unit-test.js
+++ test/unit-test.js
@@ -1,22 +1,36 @@
 const { assert, expect } = require("chai")
 const { ethers } = require("hardhat")
 
-//can run only this file by running on shell the command:
-//yarn mocha test/unit-test.js
-
+//yarn hardhat test
 describe("testing title", async () => {
   let TDMContract
 
+  //creating contract should be before each test to always experiment individually
   beforeEach(async () => {
-    TDMContractFactory = await ethers.getContractFactory(
-      "TokenDistrubutionMechanism"
-    )
-
+    TDMContractFactory = await ethers.getContractFactory("TokenDistrubutionMechanism")
     TDMContract = await TDMContractFactory.deploy()
+    // const [owner] = await ethers.getSigners();
+    // console.log('Signer 1 address: ', owner.address);
+    // return { faucet, owner };
   })
 
   it("check name", async () => {
     const name = await TDMContract.name()
     assert.equal(name, "TokenDistrubutionMechanism")
+  })
+
+  it("check symbol", async () => {
+    const symbol = await TDMContract.symbol()
+    assert.equal(symbol, "TDM")
+  })
+
+  it("check total supply", async () => {
+    const totalSupply = await TDMContract.totalSupply()
+    assert.equal(totalSupply, 10 * 10 ** 18)
+  })
+
+  it("check decimals", async () => {
+    const decimals = await TDMContract.decimals()
+    assert.equal(decimals, 18)
   })
 })
