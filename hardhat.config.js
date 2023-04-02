fa987569dc4a7154394a7f1072d6d147bc921b76
jaimebarrancos <jaime.barrancos@outlook.com>
2023-04-02 10:11:04 +0100

    Implemented gas reporter and environment variables

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("hardhat-deploy")
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  gasReporter: {
+    enabled: process.env.REPORT_GAS,
+    //currency: "USD",
+    //outputFile: "gas-report.txt",
+    //noColors: true,
+    // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
   namedAccounts: {
     deployer: {
       default: 0, // here this will by default take the first account as deployer
