047dc8579a99e8fb6b5c8870e94e772568bde07d
jaimebarrancos <jaime.barrancos@outlook.com>
2023-04-17 15:26:20 +0100

    Goerli test chain compatibiliy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("hardhat-deploy")
-//require("dotenv").config()
-//console.log(process.env)
+require("dotenv").config()
+console.log(process.env)
 const INNITIAL_SUPPLY = 10 * 10 ** 18
 
-// task("accounts", "Prints the list of accounts", async () => {
-//   const accounts = await ethers.getSigners()
-
-//   for (const account of accounts) {
-//     console.log(account.address)
-//   }
-// })
+task("accounts", "Prints the list of accounts", async () => {
+  console.log(process.env)
+})
 
 module.exports = {
   gasReporter: {
@@ -41,8 +37,11 @@ module.exports = {
       chainId: 31337,
     },
     goerli: {
-      url: "http://127.0.0.1:8545/",
-      forking: { url: process.env.ALCHEMY_GOERLI_RPC_URL },
+      url: process.env.ALCHEMY_GOERLI_RPC_URL,
+
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+
+      //forking: { url: process.env.ALCHEMY_GOERLI_RPC_URL },
       //    accounts: {
       //     mnemonic: MNEMONIC
       //   },
