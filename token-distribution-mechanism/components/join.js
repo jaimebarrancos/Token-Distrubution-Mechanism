04ac07077824dac67a444f73e81ac315dafa2403
jaimebarrancos <jaime.barrancos@outlook.com>
2023-04-27 21:16:08 +0100

    Called a contract function successfully!
    called name()
    Problem was I was accessing the wrong contract because I hadn't deployed the right one!

--- token-distribution-mechanism/components/join.js
+++ token-distribution-mechanism/components/join.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import { MissingStaticPage } from "next/dist/shared/lib/utils"
-import { useMoralisWeb3Api, useWeb3Contract, useMoralis } from "react-moralis"
+import { useWeb3ExecuteFunction, useMoralis, useApiContract } from "react-moralis"
 import abi from "../constants/abi.json"
 import contractAddresses from "../constants/contractAddresses.json"
 import { ethers } from "ethers"
@@ -9,28 +9,43 @@ export default function Join() {
   const { chainId: chainIdHex, isWeb3Enabled, account } = useMoralis()
   const chainId = parseInt(chainIdHex)
   const purejson = { 31337: ["0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"] }
+
   const [entranceFee, setEntranceFee] = useState("0")
 
-  console.log("--current account ->" + account)
+  // async function updateUI() {
+  //   console.log("updating UI" + (await joinMechanism()))
+  //   let entranceFeeCall = (await joinMechanism()).toString()
+  //   setEntranceFee(entranceFeeCall)
+  // }
 
-  const { runContractFunction: joinMechanism } = useWeb3Contract()
-  let hey = {
-    abi: abi,
-    contractAddress: purejson["31337"],
-    functionName: "pointsOf",
-    params: { account },
-    msgValue: 1 * 10 ** 8,
+  // useEffect(() => {
+  //   if (isWeb3Enabled) {
+  //     //updateUI()
+  //   }
+  // }, [isWeb3Enabled])
+
+  async function simpleUpdateUI() {
+    console.log("error message:   " + error)
+    console.log("updating UI:   " + data)
+    console.log("current account:   " + account)
   }
-  useEffect(() => {
-    if (isWeb3Enabled) {
-      async function updateUI() {
-        console.log("updating UI" + joinMechanism({ params: hey }))
-        let entranceFeeCall = (await joinMechanism()).toString()
-        setEntranceFee(entranceFeeCall)
-      }
-      updateUI()
-    }
-  }, [isWeb3Enabled])
 
-  return <div>hey {entranceFee} </div>
+  const { data, error, fetch, isFetching, isLoading } = useWeb3ExecuteFunction({
+    abi: abi,
+    contractAddress: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+    functionName: "name",
+    params: {},
+  })
+
+  return (
+    <div>
+      <div>
+        <button onClick={fetch} disabled={isFetching}>
+          Fetch data
+        </button>
+        {data && <pre>{JSON.stringify(data)}</pre>}
+      </div>
+      <button onClick={simpleUpdateUI}>print data</button>
+    </div>
+  )
 }
