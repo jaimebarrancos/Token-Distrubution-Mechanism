93407c9054528dc2e58bfeace50e8a2184fe6542
jaimebarrancos <jaime.barrancos@outlook.com>
2023-04-30 01:00:24 +0100

    Added header and entry cost call

--- token-distribution-mechanism/components/join.js
+++ token-distribution-mechanism/components/join.js
@@ -4,13 +4,13 @@ import { useWeb3ExecuteFunction, useMoralis, useApiContract } from "react-morali
 import abi from "../constants/abi.json"
 import contractAddresses from "../constants/contractAddresses.json"
 import { ethers } from "ethers"
+import { Loading } from "web3uikit"
 
 export default function Join() {
   const { chainId: chainIdHex, isWeb3Enabled, account } = useMoralis()
   const chainId = parseInt(chainIdHex)
-  const purejson = { 31337: ["0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"] }
 
-  const [entranceFee, setEntranceFee] = useState("0")
+  const [entryFee, setEntryFee] = useState("0")
 
   // async function updateUI() {
   //   console.log("updating UI" + (await joinMechanism()))
@@ -25,15 +25,18 @@ export default function Join() {
   // }, [isWeb3Enabled])
 
   async function simpleUpdateUI() {
-    console.log("error message:   " + error)
+    console.log("\n ------LOGS----- \n \nerror message:   " + error)
     console.log("updating UI:   " + data)
     console.log("current account:   " + account)
+    console.log("is loading?:   " + isLoading)
+    let newEntryFee = data.toString()
+    setEntryFee(newEntryFee)
   }
 
   const { data, error, fetch, isFetching, isLoading } = useWeb3ExecuteFunction({
     abi: abi,
-    contractAddress: contractAddresses[chainId][0],
-    functionName: "testNumber",
+    contractAddress: "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512", //contractAddresses[chainId][0],
+    functionName: "getBaseEntryFee",
     params: {},
   })
 
@@ -43,7 +46,7 @@ export default function Join() {
         <button onClick={fetch} disabled={isFetching}>
           Fetch data
         </button>
-        {data && <pre>{JSON.stringify(data)}</pre>}
+        Entry value is: {isLoading ? "loading..." : entryFee}
       </div>
       <button onClick={simpleUpdateUI}>print data</button>
     </div>
--- token-distribution-mechanism/src/pages/_app.js
+++ token-distribution-mechanism/src/pages/_app.js
@@ -1,13 +1,17 @@
 import "@/styles/globals.css"
 import { MoralisProvider } from "react-moralis"
 import { NotificationProvider } from "web3uikit"
+import { Routes, Route } from "react-router-dom"
+import About from "./about.js"
 
 export default function App({ Component, pageProps }) {
   return (
-    <MoralisProvider initializeOnMount={false}>
-      <NotificationProvider>
-        <Component {...pageProps} />
-      </NotificationProvider>
-    </MoralisProvider>
+    <>
+      <MoralisProvider initializeOnMount={false}>
+        <NotificationProvider>
+          <Component {...pageProps} />
+        </NotificationProvider>
+      </MoralisProvider>
+    </>
   )
 }
